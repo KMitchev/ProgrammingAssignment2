0c08e8db8240614d76d748997424d55e1e3474a5
Kiril Mitchev <k.mitchev@gmail.com>
2014-09-21 15:38:30 +0300

    edit cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The first function "makeCacheMatrix" creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## set the value of the matrix
+    m<-NULL
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    ## get the value of the matrix
+    get<-function() x
+    ## set the inverse of the matrix
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+    
+    ## get the inverse of the matrix
+    list(set=set, get=get,
+         setmatrix=setmatrix,
+         getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    ## get the inverse of the matrix  
+    m<-x$getmatrix()
+    
+    ## check if there is the inverse is already been calculated
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    ## if not: get the inverse 
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    ## set the inverse
+    x$setmatrix(m)
+    m
 }
---
+++ new.md
@@ -0,0 +1,14 @@
+# This is the header
+
+Here is some regular text
+
+* bullet 1
+
+* bullet 2
+
+
+[link to Google](https://www.google.bg/?gws_rd=ssl)
+
+
+Some more text
+
